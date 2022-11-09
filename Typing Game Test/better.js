de86208eea326008d07b69db43eba9c408b0ed9b
alangd707 <111930791+alangd707@users.noreply.github.com>
2022-11-09 00:12:15 -0700

    Prototype of typing game established
    - Still needs to be a feature that updates the text that displays the current progress of the user.

--- Typing Game Test/better.js
+++ Typing Game Test/better.js
@@ -1,31 +1,66 @@
 let word = wordDecider()
-console.log(word)
 let playerInput = [null,null,null,null,null]
+let correctLetters = [null,null,null,null,null]
+let valid = false;
 
 window.addEventListener('keydown', function(keyPressed) {
     if (keyPressed.code.includes('Key')) {
         let letter = keyPressed.code.replace('Key', '');
         if (playerInput.includes(null)) {
             playerInput[playerInput.indexOf(null)] = letter;
-            console.log(playerInput)
+            console.log(playerInput);
         }
     }
     if (keyPressed.code === 'Enter' && !playerInput.includes(null)) {
-        let normalWord = JSON.stringify(playerInput)
-        loadJSON('https://api.dictionaryapi.dev/api/v2/entries/en/')
-        checkPlayerInput()
+        let playerWordString = playerInput.toString().replaceAll(',', '');
+        checkIfInputIsValid(playerWordString)
+        if (valid == true) {
+            checkSuccess()
+            resetInput()
+            valid = false;
+        }
     }
+    if (keyPressed.code === 'Backspace') {
+        if (playerInput.includes(null)) {
+            playerInput[playerInput.indexOf(null) - 1] = null;
+        } else {
+            playerInput[4] = null; 
+        }
+        console.log(playerInput);
+    }
+    updateText()
 })
 
-function checkPlayerInput() {
-    let correctLetters = [null,null,null,null,null];
+function updateText() {
+    
+}
 
+function checkSuccess() {
     for (let i = 0; i < 5; i++) {
         for (let a = 0; a < 5; a++) {
             if (word[i] === playerInput[a]) {
-                correctLetters[i] = playerInput[a]
+                correctLetters[i] = playerInput[a];
             }
         }
     }
-    console.log(correctLetters)
+    console.log(correctLetters);
+    if (!correctLetters.includes(null)) {
+        console.log('Success!')
+    }
+}
+
+function resetInput() {
+    playerInput = [null,null,null,null,null];
+}
+
+function checkIfInputIsValid(x) {
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${x}`;
+    let http = new XMLHttpRequest();
+    http.open('HEAD', url, true);
+    http.send();
+    if (http.status != 404) {
+        valid = true;
+    } else {
+        valid = false;
+    }
 }
