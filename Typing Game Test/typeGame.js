130659ebd96c945781713fe95234630d096abd99
Outerspace555 <111930791+alangd707@users.noreply.github.com>
2022-11-29 18:18:43 -0700

    Fully functional word game

--- Typing Game Test/typeGame.js
+++ Typing Game Test/typeGame.js
@@ -1,25 +1,23 @@
 const userInputBox = document.getElementById('user-input')
 const reqLabel = document.getElementById('requirement-spam')
-const randomWordDatabase = [
-    'WHITEBAG',
-    'DISESTABLISHMENTARIANISM',
-    'FROGGO',
-    'ANDREW',
-    'BRENARD',
-    'TERRARIA',
-    'TESTISTIFICATE',
-    'UNICORN',
-    'AGUARIAN',
-    'AGRARIAN',
-    'UNITED',
-    'PNEUMONOULTRAMICROSCOPICSILICOVOLCANOCONIOSIS',
-    'CHUGNIS'
-]
+let points = 0; let lives = 5; let rounds = 0; let wordRequirements
 
 function checkInput(input) {
-    if(checkIfWordExists(input)) {
+    if(checkIfWordMeetsReqs(input) && checkIfWordExists(input)) {
+        console.log('Game won!')
+        points += input.length
+    } else {lives--}
+    newRound()
+}
 
-    }
+function newRound() {
+    determineReqs()
+    rounds++
+    document.getElementById('points-spam').innerHTML = points;
+    document.getElementById('rounds-spam').innerHTML = rounds;
+    document.getElementById('lives-spam').innerHTML = lives;
+    userInputBox.value = ''
+    reqLabel.innerHTML = wordRequirements.toLowerCase()
 }
 
 let checkIfWordExists = (x) => {
@@ -34,13 +32,41 @@ let checkIfWordExists = (x) => {
     }
 }
 
-let checkIfMeetsReqs = (x) => {
+let checkIfWordMeetsReqs = (x) => { if (x.includes(wordRequirements)) {return true} else {return false} }
 
+function determineReqs() {
+    let randomLetters = () => {
+        const randomWordDatabase = [
+            'WHITEBAG',
+            'DISESTABLISHMENTARIANISM',
+            'FROGGO',
+            'ANDREW',
+            'BRENARD',
+            'TERRARIA',
+            'TESTISTIFICATE',
+            'UNICORN',
+            'AGRARIAN',
+            'UNITED',
+            'PNEUMONOULTRAMICROSCOPICSILICOVOLCANOCONIOSIS',
+            'CHUGNIS',
+            'THE',
+            'GREATEST',
+            'GAME',
+            'OF',
+            'ALL',
+            'TIME',
+            'APPLE',
+            'NORTH',
+            'KOREA',
+            'SUPERCALIFRAGILISTICEXPIALIDOCIOUS',
+            'ARIZONA'
+        ]
+        let determineWord = () => {return randomWordDatabase[Math.floor(Math.random() * randomWordDatabase.length)].split('')}
+        let wordArray = determineWord()
+        let randomIndex = Math.floor(Math.random() * (wordArray.length - 1))
+        return wordArray[randomIndex] + wordArray[randomIndex + 1]
+    }
+    wordRequirements = randomLetters();
 }
 
-let randomLetters = (x) => {
-    let determineWord = () => {return randomWordDatabase[Math.floor(Math.random() * randomWordDatabase.length)].split('')}
-    let wordArray = determineWord()
-    return wordArray[Math.floor(Math.random() * (wordArray.length - 1))]
-}
-console.log(randomLetters())
+newRound()
