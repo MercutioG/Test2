a6e3453d7f5e33265038eed781c2e3c69647958b
alangd707 <111930791+alangd707@users.noreply.github.com>
2023-01-17 19:27:29 -0700

    Start of final product
    Started actual game process, needed method for player to be able to attack, more enemies, and game over screen as well as use of difficulty. Comments go more indepth.

--- main/game.js
+++ main/game.js
@@ -1,10 +1,73 @@
 class Game {
     constructor(difficulty, enemy) {
-        this.mob = enemy;
+        this.player = window.player;
+        this.enemy = enemy;
+        this.enemyHP = this.enemy.maxHP;
         this.difficulty = difficulty;
-        this.playerHP = player.maxHP;
+        this.playerHP = this.player.maxHP;
+        this.wordReqs = null;
+        this.timerFunction = null;
+        this.currentTimer = null;
+        this.enemiesKilled = 0; // Builds in base variables needed for storing information about the game for further use.
     }
     init() {
         window.gui.toggleWindow('fight-display')
+        this.roundStart();
+    } // Toggles screen from start screen to game screen, as well as starting the actual game loop.
+    roundStart() {
+        this.roundLoop()
+        game.timerFunction = setInterval(game.timerTick, 1000); //Sets the game loop.
+    }
+    roundLoop() {
+        game.currentTimer = 10;
+        this.determineWordReqs()
+        this.checkGameState() //Resets timer back to 10 and generates new requirements for user.
+    }
+    timerTick() {
+        game.currentTimer -= 1;
+        if (game.currentTimer === 0) {
+            game.enemyAttack();
+            game.roundLoop();
+        }
+        game.checkGameState()
+    } //Ticks the timer, checks if timer reaches 0. If timer reaches 0, the enemy will attack and the round will reset.
+    playerAttack() {
+        if (game.mobHP - game.player.baseDamage <= 0) {
+            game.mobHP = 0
+        } else {
+            game.mobHP -= game.player.baseDamage
+        }
+        this.checkGameState()
+    } // Method that makes mob's HP decrease by the player's base attack.
+    enemyAttack() {
+        if (game.playerHP - game.enemy.baseDamage <= 0) {
+            game.enemyHP = 0
+        } else {
+            game.enemyHP -= game.player.baseDamage
+        }
+        this.checkGameState()
+    } // Like player attack but for when the enemy attacks.
+    checkGameState() {
+        if (playerHP === 0) {
+            this.gameOver()
+        }
+        this.updateInterface()
+    } //Checks if game has been decided, such as if player dies or player kills mob.
+    determineWordReqs() {
+        this.wordReqs = "TT"
+    }
+    gameOver() {}
+    gameWon() {}
+    updateInterface() {
+        let requirementsLabel = document.getElementById("requirements-label")
+        let timerLabel = document.getElementById("timer-label")
+        let playerHealthLabel = document.getElementById("player-health-label")
+        let enemyHealthLabel = document.getElementById("enemey-health-label")
+        let enemiesKilledLabel = document.getElementById("total-kills-label")
+        requirementsLabel.innerHTML = this.wordReqs;
+        timerLabel.innerHTML = this.currentTimer;
+        playerHealthLabel.innerHTML = this.playerHP;
+        enemyHealthLabel.innerHTML = this.enemyHP;
+        enemiesKilledLabel.innerHTML = this.enemiesKilled;
     }
 }
--- main/typeGame.js
+++ main/typeGame.js
@@ -1,88 +1,86 @@
+const userInputBox = document.getElementById('user-input');
+const reqLabel = document.getElementById('requirement-spam');
+let points = 0; let bossHP = 100; let rounds = 0; let wordRequirements; let playerHP = 100; let timer = 10;
 
+function checkInput(input) {
+    if(checkIfWordMeetsReqs(input) && checkIfWordExists(input)) { 
+        points += Math.round(input.length**1.5 + 3);
+        bossHP -= points;
+    } else {playerHP -= 3};
+    newRound();
+}
 
-    const userInputBox = document.getElementById('user-input');
-    const reqLabel = document.getElementById('requirement-spam');
-    let points = 0; let bossHP = 100; let rounds = 0; let wordRequirements; let playerHP = 100; let timer = 10;
+function newRound() {
+    determineReqs();
+    timer = 10;
+    callTimer();
+    rounds++;
+    document.getElementById('points-spam').innerHTML = points;
+    document.getElementById('rounds-spam').innerHTML = rounds;
+    document.getElementById('lives-spam').innerHTML = bossHP;
+    document.getElementById('health-spam').innerHTML = playerHP;
+    userInputBox.value = '';
+    reqLabel.innerHTML = wordRequirements.toLowerCase();
+}
 
-    function checkInput(input) {
-        if(checkIfWordMeetsReqs(input) && checkIfWordExists(input)) { 
-            points += Math.round(input.length**1.5 + 3);
-            bossHP -= points;
-        } else {playerHP -= 3};
-        newRound();
+let checkIfWordExists = (x) => {
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${x}`;
+    let http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    if (http.status != 404) {
+        return true;
+    } else {
+        return false;
     }
+}
 
-    function newRound() {
-        determineReqs();
-        timer = 10;
-        callTimer();
-        rounds++;
-        document.getElementById('points-spam').innerHTML = points;
-        document.getElementById('rounds-spam').innerHTML = rounds;
-        document.getElementById('lives-spam').innerHTML = bossHP;
-        document.getElementById('health-spam').innerHTML = playerHP;
-        userInputBox.value = '';
-        reqLabel.innerHTML = wordRequirements.toLowerCase();
+let checkIfWordMeetsReqs = (x) => { if (x.includes(wordRequirements)) {return true} else {return false} };
+
+function determineReqs() {
+    let randomLetters = () => {
+        const randomWordDatabase = [
+            'WHITEBAG',
+            'ANTIDISESTABLISHMENTARIANISM',
+            'FROGGO',
+            'ANDREW',
+            'BRENARD',
+            'TERRARIA',
+            'TESTISTIFICATE',
+            'UNICORN',
+            'AGRARIAN',
+            'UNITED',
+            'PNEUMONOULTRAMICROSCOPICSILICOVOLCANOCONIOSIS',
+            'CHUGNIS',
+            'THE',
+            'GREATEST',
+            'GAME',
+            'OF',
+            'ALL',
+            'TIME',
+            'APPLE',
+            'NORTH',
+            'BLANKETS',
+            'SUPERCALIFRAGILISTICEXPIALIDOCIOUS',
+            'ARIZONA'
+        ]
+        let determineWord = () => {return randomWordDatabase[Math.floor(Math.random() * randomWordDatabase.length)].split('')}
+        let wordArray = determineWord()
+        let randomIndex = Math.floor(Math.random() * (wordArray.length - 1))
+        return wordArray[randomIndex] + wordArray[randomIndex + 1]
     }
-
-    let checkIfWordExists = (x) => {
-        let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${x}`;
-        let http = new XMLHttpRequest();
-        http.open('HEAD', url, false);
-        http.send();
-        if (http.status != 404) {
-            return true;
+    wordRequirements = randomLetters();
+}
+function callTimer() {
+    downloadTimer = setInterval(function(){
+        if(timer <= 0){
+        clearInterval(downloadTimer);
+        playerHP -= 3;
+        newRound()
         } else {
-            return false;
+        timer -= 1;
         }
-    }
-
-    let checkIfWordMeetsReqs = (x) => { if (x.includes(wordRequirements)) {return true} else {return false} };
-
-    function determineReqs() {
-        let randomLetters = () => {
-            const randomWordDatabase = [
-                'WHITEBAG',
-                'ANTIDISESTABLISHMENTARIANISM',
-                'FROGGO',
-                'ANDREW',
-                'BRENARD',
-                'TERRARIA',
-                'TESTISTIFICATE',
-                'UNICORN',
-                'AGRARIAN',
-                'UNITED',
-                'PNEUMONOULTRAMICROSCOPICSILICOVOLCANOCONIOSIS',
-                'CHUGNIS',
-                'THE',
-                'GREATEST',
-                'GAME',
-                'OF',
-                'ALL',
-                'TIME',
-                'APPLE',
-                'NORTH',
-                'BLANKETS',
-                'SUPERCALIFRAGILISTICEXPIALIDOCIOUS',
-                'ARIZONA'
-            ]
-            let determineWord = () => {return randomWordDatabase[Math.floor(Math.random() * randomWordDatabase.length)].split('')}
-            let wordArray = determineWord()
-            let randomIndex = Math.floor(Math.random() * (wordArray.length - 1))
-            return wordArray[randomIndex] + wordArray[randomIndex + 1]
-        }
-        wordRequirements = randomLetters();
-    }
-    function callTimer() {
-        downloadTimer = setInterval(function(){
-            if(timer <= 0){
-            clearInterval(downloadTimer);
-            playerHP -= 3;
-            newRound()
-            } else {
-            timer -= 1;
-            }
-            document.getElementById("timer").innerHTML = timer;
-        }, 1000);
-    }
-    newRound()
+        document.getElementById("timer").innerHTML = timer;
+    }, 1000);
+}
+newRound()
